6fad81f0ead3a738333f868c450a85bb8da4fdc3
Jardiano12 <jardiano.ifrr.tcc@gmail.com>
2019-12-22 20:23:17 -0300

    Conversas criadas

---
+++ lib/model/chat_model.dart
@@ -0,0 +1,50 @@
+
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String qtd;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.qtd, this.avatarUrl});
+}
+
+  List<ChatModel> dummyData = [
+    new ChatModel(
+        name: "Rodrigo Marinho",
+        message: "Ei pow, Bora Tomar uma ?",
+        time: "15:30",
+        qtd: "55",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png"),
+    new ChatModel(
+        name: "Tiago",
+        message: "Mais uma acalorada, topa ?",
+        time: "15:30",
+        qtd: "1",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png"),
+    new ChatModel(
+        name: "Moises",
+        message: "O nome da série é The Witcher",
+        time: "15:30",
+        qtd: "1",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png"),
+    new ChatModel(
+        name: "Julio",
+        message: "Hoje eu atropelei 3 gatos na estrada",
+        time: "15:30",
+        qtd: "1",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png"),
+    new ChatModel(
+        name: "Vinicios",
+        message: "Vamos pra igreja minimizar teus pecados kkk",
+        time: "15:30",
+        qtd: "1",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png"),
+    new ChatModel(
+        name: "Marcos",
+        message: "Cadê o curso seu corrupto?",
+        time: "15:30",
+        qtd: "1",
+        avatarUrl: "https://image.flaticon.com/icons/png/512/149/149071.png")
+  ];
+
--- lib/ui/chat_screen.dart
+++ lib/ui/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../model/chat_model.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -8,10 +9,56 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context){
-    return new Container(
-        child: new Center(
-          child: new Text('Chat', style: new TextStyle(fontSize: 20.0),),
-        )
+    return new ListView.builder(
+      itemCount: dummyData.length,
+      itemBuilder: (context, i) => Column(
+        children: <Widget>[
+          new Divider(
+            height: 10.0,
+          ),
+          new ListTile(
+            leading: new CircleAvatar(
+              foregroundColor: Theme.of(context).primaryColor,
+              backgroundColor: Colors.grey,
+              backgroundImage: new NetworkImage(dummyData[i].avatarUrl),
+            ),
+            title: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Text(
+                  dummyData[i].name,
+                  style:  new TextStyle(fontWeight: FontWeight.bold,),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Text(
+                      dummyData[i].time,
+                      style:  new TextStyle(color: Colors.grey, fontSize: 14.0, fontWeight: FontWeight.bold,),
+                    ),
+//                    SizedBox(height: 10.0,),
+                    CircleAvatar(
+                      backgroundColor: Theme.of(context).accentColor,
+                      radius: 10.0,
+                      child:new Text(
+                        dummyData[i].qtd,
+                        style:  new TextStyle(color: Colors.white, fontSize: 10.0, fontWeight: FontWeight.bold,),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+            subtitle: new Container(
+              padding: const EdgeInsets.only(top: 5.0),
+              child:new Text(
+                dummyData[i].message,
+                style:  new TextStyle(fontWeight: FontWeight.normal, fontSize: 15.0,),
+              ),
+            ),
+          )
+        ],
+      )
     );
   }
 }
